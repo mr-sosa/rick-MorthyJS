c38e73664ffd4a3cf163080bfacd80ab72a0cbea
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-02-24 13:46:33 -0500

    Card flex

--- js/script.js
+++ js/script.js
@@ -12,9 +12,29 @@ async function getData() {
 async function render(){
     const data = await getData();
     const dataMapped = data.results.map((character) => {
-        const p = document.createElement('p');
-        p.textContent = character.name;
-        containerRef.appendChild(p)
+      const div = document.createElement('div');
+      div.className = 'card text-white bg-dark mb-3';
+
+      const image = document.createElement('img');
+      image.className = 'card-img-top';
+      image.src = character.image;
+      div.appendChild(image);
+
+      const body = document.createElement('div');
+      body.className = 'card-body';
+
+      const name = document.createElement('h5');
+      name.textContent = character.name;
+      name.className = 'card-title';
+      body.appendChild(name);
+
+      const parrafo = document.createElement('p');
+      parrafo.textContent = 'Specie: '+ character.species + ' - Gender: ' + character.gender;
+      parrafo.className = 'card-text';
+      body.appendChild(parrafo);
+
+      div.appendChild(body);
+      containerRef.appendChild(div);
     })
 }
 
